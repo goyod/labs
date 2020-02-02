e528f1551eeb8dc9cbca4f71d6ecf573f4041910
pallat <yod.pallat@gmail.com>
2020-02-02 23:45:16 +0700

    jwt

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/goyod/labs
 go 1.13
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.5.0
 	github.com/gorilla/mux v1.7.3
 )
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
+	"time"
 
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"github.com/goyod/labs/fizzbuzz"
 )
@@ -14,7 +17,43 @@ func main() {
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
+func tokenHandler(c *gin.Context) {
+	mySigningKey := []byte("AllYourBase")
+
+	claims := &jwt.StandardClaims{
+		ExpiresAt: time.Now().Add(time.Minute * 5).Unix(),
+		Issuer:    "pallat",
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	ss, err := token.SignedString(mySigningKey)
+	if err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.String(http.StatusOK, ss)
+}
+
 func fizzbuzzHandler(c *gin.Context) {
+	tokenString := c.GetHeader("Authorization")[7:]
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte("AllYourBase"), nil
+	})
+
+	if claims, ok := token.Claims.(jwt.MapClaims); !ok || !token.Valid {
+		c.String(http.StatusUnauthorized, err.Error())
+		_ = claims
+		return
+	}
+
 	n, err := strconv.Atoi(c.Param("number"))
 	if err != nil {
 		c.String(http.StatusBadRequest, err.Error())
