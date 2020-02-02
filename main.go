b3630f7e9731ffe451c260bb843f4ff2cb9c084f
pallat <yod.pallat@gmail.com>
2020-02-02 23:34:48 +0700

    gorilla FizzBuzz

--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/goyod/labs/fizzbuzz"
 )
 
 func main() {
-	for i := 1; i <= 100; i++ {
-		fmt.Print(fizzbuzz.FizzBuzz(i) + ",")
+	r := mux.NewRouter()
+	r.HandleFunc("/fizzbuzz/{number}", fizzbuzzHandler)
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func fizzbuzzHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	n, err := strconv.Atoi(vars["number"])
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
+
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, fizzbuzz.FizzBuzz(n))
 }
